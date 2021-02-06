38ec5ab33eff6f55179644fe725889cd8272b1a8
Vitalii <vitaliy.rysich@gmail.com>
2021-02-06 14:47:53 +0200

    Added @CrossOrigin

--- src/main/java/com/todo/controller/HelloWorldController.java
+++ src/main/java/com/todo/controller/HelloWorldController.java
@@ -1,13 +1,10 @@
 package com.todo.controller;
 
 import com.todo.dto.HelloWorldBean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping
 public class HelloWorldController {
 
     @GetMapping(path = "/hello-world")

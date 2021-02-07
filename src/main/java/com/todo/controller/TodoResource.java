b928cf0e747ec328b8ef59ba0335307029bf6145
Vitalii <vitaliy.rysich@gmail.com>
2021-02-07 20:34:27 +0200

    Update Todo

--- src/main/java/com/todo/controller/TodoResource.java
+++ src/main/java/com/todo/controller/TodoResource.java
@@ -2,9 +2,12 @@ package com.todo.controller;
 
 import com.todo.dto.Todo;
 import com.todo.service.TodoHardcodedService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -22,6 +25,11 @@ public class TodoResource {
         return todoService.findAll();
     }
 
+    @GetMapping(path = "/users/{username}/todos/{id}")
+    public Todo getTodo(@PathVariable String username, @PathVariable long id){
+        return todoService.findById(id);
+    }
+
     @DeleteMapping(path = "/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
         Todo todo = todoService.deleteById(id);
@@ -30,6 +38,29 @@ public class TodoResource {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+
+    @PutMapping(path = "/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(
+            @PathVariable String username,
+            @PathVariable long id,
+            @RequestBody Todo todo) {
+
+        Todo todoUpdated = todoService.save(todo);
+        return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/users/{username}/todos")
+    public ResponseEntity<Void> createTodo(
+            @PathVariable String username,
+            @RequestBody Todo todo) {
+
+        Todo todoCreated = todoService.save(todo);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(todoCreated.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
